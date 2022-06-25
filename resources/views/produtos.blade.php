b462a5cc74ba22f59d2254d726cfd0b5faff6211
Brunnokauan <brunno_kauan@hotmail.com>
2022-06-25 13:11:19 -0300

    Second commit

---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'Prodtos')
+
+@section('content')
+    
+@endsection
+
+<h1>Tela de Produtos</h1>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('title', 'HDC Events')
+
+@section('content')
+
+<h1>Algum Título</h1>
+    
+    
+@endsection
