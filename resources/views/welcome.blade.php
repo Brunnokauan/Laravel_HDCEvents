19d4d8ae7ebb577ef474ed797c683de3714f5943
Brunnokauan <brunno_kauan@hotmail.com>
2022-07-12 10:13:01 -0300

    Add and save image in events

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class="card col-md-3">
-            <img src="/img/banner2.jpg" alt="{{ $event->title }}">
+            <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
             <div class="card-body">
                 <p class="card-date">11/11/1111</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
