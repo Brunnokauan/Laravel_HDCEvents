e5eb8dffae027b69dfc32e562fbf0526739d65c0
Brunnokauan <brunno_kauan@hotmail.com>
2022-07-20 11:41:13 -0300

    Criado dashboard

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -65,7 +66,18 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
-        return view('events.show', ['event' => $event]);
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+
+    }
+
+    public function dashboard() {
+
+        $user = auth()->user();
+
+        $events = $user->events;
+
+        return view('events.dashboard', ['events' => $events]);
     }
 }
